a28ec739d4445fcf8442a798065f6c9ad8b6dc7b
Gangaraj Siddhaarth Raj <gangaraj_siddhaarthraj@epam.com>
2024-08-21 18:03:08 +0530

    initial commit

---
+++ src/main/java/com/epam/travelrecommendation/TravelrecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.epam.travelrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelrecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelrecommendationApplication.class, args);
+	}
+
+}
